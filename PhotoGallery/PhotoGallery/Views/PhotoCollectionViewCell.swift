f4d50df17224e3091906ad8f4cbb57d9e6ee89c1
reem hesham <reem.hesham@modeso.ch>
2018-07-28 18:35:19 +0200

    Implement PhotoCollectionView custom cell

---
+++ PhotoGallery/PhotoGallery/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoGallery
+//
+//  Created by reem hesham on 7/28/18.
+//  Copyright © 2018 reem hesham. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+}
