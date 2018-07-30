71b33ddca6e42f539ebd2f382d5d193cd404ea81
reem hesham <reem.hesham@modeso.ch>
2018-07-31 01:00:31 +0200

    Implement pinch to zoom functionality
    Apply pinch to zoom functionality to the full photo view

--- PhotoGallery/PhotoGallery/ViewControllers/PhotoDetailsViewController.swift
+++ PhotoGallery/PhotoGallery/ViewControllers/PhotoDetailsViewController.swift
@@ -12,11 +12,15 @@ import Kingfisher
 class PhotoDetailsViewController: UIViewController {
 
     @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var photoDetailsViewModel: PhotoDetailsViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 6.0
 
         photoImage.kf.setImage(with: photoDetailsViewModel?.getPhotoUrl(), placeholder: #imageLiteral(resourceName: "Placeholder"), options: nil, progressBlock: nil, completionHandler: nil)
     }
@@ -28,5 +32,33 @@ class PhotoDetailsViewController: UIViewController {
 
     func config(_ viewModel: PhotoDetailsViewModel) {
         photoDetailsViewModel = viewModel
+    }
+}
+extension PhotoDetailsViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoImage
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            
+            if let image = photoImage.image {
+                
+                let ratioW = photoImage.frame.width / image.size.width
+                let ratioH = photoImage.frame.height / image.size.height
+                
+                let ratio = ratioW < ratioH ? ratioW:ratioH
+                
+                let newWidth = image.size.width*ratio
+                let newHeight = image.size.height*ratio
+                
+                let left = 0.5 * (newWidth * scrollView.zoomScale > photoImage.frame.width ? (newWidth - photoImage.frame.width) : (scrollView.frame.width - scrollView.contentSize.width))
+                let top = 0.5 * (newHeight * scrollView.zoomScale > photoImage.frame.height ? (newHeight - photoImage.frame.height) : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsetsMake(top, left, top, left)
+            }
+        } else {
+            scrollView.contentInset = .zero
+        }
     }
 }
