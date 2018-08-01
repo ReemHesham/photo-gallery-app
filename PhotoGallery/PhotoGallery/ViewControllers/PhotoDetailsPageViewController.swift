109c65221de280835816540f3bcc5dcca32588e5
reem hesham <reem.hesham@modeso.ch>
2018-08-01 16:49:59 +0200

    Add PhotoSlider
    Create PhotoSlider storyboard, create PageViewController and move PhotoDetailsViewController form Main storyboard to PhotoSlider storyboard
    Create PhotoDetailsPageViewController
    Create PhotoPageViewModel

---
+++ PhotoGallery/PhotoGallery/ViewControllers/PhotoDetailsPageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDetailsPageViewController.swift
+//  PhotoGallery
+//
+//  Created by reem hesham on 8/1/18.
+//  Copyright © 2018 reem hesham. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsPageViewController: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ PhotoGallery/PhotoGallery/ViewModels/PhotoPageViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoPageViewModel.swift
+//  PhotoGallery
+//
+//  Created by reem hesham on 8/1/18.
+//  Copyright © 2018 reem hesham. All rights reserved.
+//
+
+import Foundation
+
+class PhotoPageViewModel {
+}
