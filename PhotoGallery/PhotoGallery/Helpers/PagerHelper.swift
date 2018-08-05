d374d8f04bd0a1916aca5ac2de4ac8342c13140a
reem hesham <reem.hesham@modeso.ch>
2018-08-05 21:29:49 +0200

    Edit PhotoDetailsPageViewController
    Make code refactoring in PhotoDetailsPageViewController
    Create AnimationHelper class, which responsible for handling animation
    Create PagerHelper class

---
+++ PhotoGallery/PhotoGallery/Helpers/PagerHelper.swift
@@ -0,0 +1,47 @@
+//
+//  PagerHelper.swift
+//  PhotoGallery
+//
+//  Created by reem hesham on 8/5/18.
+//  Copyright © 2018 reem hesham. All rights reserved.
+//
+
+import UIKit
+
+class PagerHelper {
+    
+    var pageViewController: UIPageViewController?
+    var currentIndex: Int = 0
+    fileprivate let photoSliderCache = NSCache<AnyObject, AnyObject>()
+    var selectedPhoto: Photo?
+    
+    init(_ pageViewController: UIPageViewController, currentIndex: Int, photo: Photo) {
+        self.pageViewController = pageViewController
+        self.currentIndex = currentIndex
+        self.selectedPhoto = photo
+        setPage(with: currentIndex, photo: photo)
+    }
+    
+    func setPage(with index:Int, photo: Photo) {
+        selectedPhoto = photo
+        if let viewController = photoViewController(for: index) {
+            pageViewController?.setViewControllers([viewController], direction: (index > currentIndex ? .forward : .reverse), animated: true, completion: nil)
+            
+            currentIndex = index
+            selectedPhoto = photo
+        }
+    }
+
+    func photoViewController(for pageIndex: Int) -> PhotoDetailsViewController? {
+        
+        if let cachedViewController = photoSliderCache.object(forKey: pageIndex as AnyObject) as? PhotoDetailsViewController {
+            return cachedViewController
+        }
+        guard let viewController = PhotoSliderRouter.instantiatePhotoDetailsViewController() as? PhotoDetailsViewController, let photo = selectedPhoto else {
+            fatalError("Unable to instantiate a PhotoDetailsViewController")
+        }
+        viewController.config(PhotoDetailsViewModel(photo))
+        photoSliderCache.setObject(viewController, forKey: pageIndex as AnyObject)
+        return viewController
+    }
+}
